d55c69db6b4ab7dc1bf67d0176f037cb3769d68a
isaccaldas <34041465+isaccaldas@users.noreply.github.com>
2022-09-03 15:46:43 -0300

    Create Variaveis.java

---
+++ JavaCodes/src/Modulo2/Variaveis.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modulo2;
+
+/**
+ *
+ * @author isacc
+ */
+public class Variaveis {
+    
+    public static void main(String[] args) {
+        
+        int numero; //Declarando uma variável
+        int numero2, numero3; //Declarando três variáveis na mesma linha
+        
+        //Inicializando variáveis
+        numero = 10;
+        numero2 = 15;
+        numero3 = 12;
+        
+        //Declarar e inicializar variáveis na mesma linha.
+        byte z = 25;
+        float numFloat = 22.50f;
+        char letra = 'a', letra2 = 'b';
+        
+        //Inicialização dinâmica
+        int total =  numero + numero2;
+        
+    }
+    
+    
+}
