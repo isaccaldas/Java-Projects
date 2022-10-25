04eb99e2d4c0d2a94d1f0bfd1d7ab1ad3d62f8e1
isaccaldas <34041465+isaccaldas@users.noreply.github.com>
2022-10-24 22:47:16 -0300

    Create BreakLoop.java

---
+++ JavaCodes/src/Modulo3/BreakLoop.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modulo3;
+
+/**
+ *
+ * @author isacc
+ */
+public class BreakLoop {
+ 
+    public static void main(String[] args) {
+     
+        for(int i = 0; i < 100; i++){
+        
+            if(i == 10) break; //finaliza o loop se i for igual a 10
+            System.out.println("i= " + i);
+        }
+        
+        System.out.println("Loop completo");
+        
+    }
+    
+    
+}
