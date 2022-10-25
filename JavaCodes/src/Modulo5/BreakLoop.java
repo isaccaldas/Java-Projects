6182ffbb82b937353a4b7aab05f09e03135f66f4
isaccaldas <34041465+isaccaldas@users.noreply.github.com>
2022-10-24 23:17:13 -0300

    ajustando o pacote correto

--- JavaCodes/src/Modulo5/BreakLoop.java
+++ JavaCodes/src/Modulo5/BreakLoop.java
@@ -2,27 +2,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Modulo3;
+package Modulo5;
 
 /**
  *
  * @author isacc
  */
-public class BreakLoop2 {
-    
-     public static void main(String[] args) {
+public class BreakLoop {
+ 
+    public static void main(String[] args) {
      
-         int i = 0;
-         
-         while(i<100){
+        for(int i = 0; i < 100; i++){
+        
             if(i == 10) break; //finaliza o loop se i for igual a 10
             System.out.println("i= " + i);
-            i++;
-         }
-        
+        }
         
         System.out.println("Loop completo");
         
     }
+    
     
 }
--- JavaCodes/src/Modulo5/BreakLoop2.java
+++ JavaCodes/src/Modulo5/BreakLoop2.java
@@ -2,25 +2,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Modulo3;
+package Modulo5;
 
 /**
  *
  * @author isacc
  */
-public class BreakLoop {
- 
-    public static void main(String[] args) {
+public class BreakLoop2 {
+    
+     public static void main(String[] args) {
      
-        for(int i = 0; i < 100; i++){
-        
+         int i = 0;
+         
+         while(i<100){
             if(i == 10) break; //finaliza o loop se i for igual a 10
             System.out.println("i= " + i);
-        }
+            i++;
+         }
+        
         
         System.out.println("Loop completo");
         
     }
-    
     
 }
