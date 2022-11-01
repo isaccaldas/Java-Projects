73ad175449151523ee69442c4c9030acfb56761d
isaccaldas <34041465+isaccaldas@users.noreply.github.com>
2022-11-01 08:34:37 -0300

    Create FinalFields.java

---
+++ JavaCodes/src/Modulo7/FinalFields.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modulo7;
+
+/**
+ *
+ * @author isacc
+ */
+public class FinalFields {
+    
+    final int NUMERO = 1; // Exemplo de uma constante. O campo deve ser inicializado quando declarado como uma constante
+    
+    
+}
