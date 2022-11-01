0e47c634fb52682fc3cd7f32ce2f39bd2d2ff89b
isaccaldas <34041465+isaccaldas@users.noreply.github.com>
2022-11-01 08:38:47 -0300

    Create ArrayRevisitado.java

---
+++ JavaCodes/src/Modulo7/ArrayRevisitado.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modulo7;
+
+/**
+ *
+ * @author isacc
+ */
+public class ArrayRevisitado {
+    
+    public static void main(String[] args) {
+        
+        int a1[] = new int [10];
+        int a2[] = {3,5,7,1,8,99,44,-10};
+        int a3[] = {4, 3, 2, 1};
+        String a4[] = {"Maria", "oi", "sim"};
+        
+        System.out.println(a1.length);
+        System.out.println(a2.length);
+        System.out.println(a3.length);
+        System.out.println(a4.length);
+        
+    }
+    
+    
+}
