5b99062694849927057aa66dd0b86e3208e03cc4
isaccaldas <34041465+isaccaldas@users.noreply.github.com>
2022-11-02 13:35:29 -0300

    varArgs

---
+++ JavaCodes/src/Modulo7/StringDemo.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modulo7;
+
+/**
+ *
+ * @author isacc
+ */
+public class StringDemo {
+    
+    
+    public static void main(String[] args) {
+        
+        String strOb1 = "Primeira String";
+        String strOb2 = "Segunda String";
+        String strOb3 = strOb1 + " and " + strOb2;
+         
+        System.out.println(strOb1);
+        System.out.println(strOb2);
+        System.out.println(strOb3);
+        
+    }
+    
+}
---
+++ JavaCodes/src/Modulo7/VarArgs.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modulo7;
+
+/**
+ *
+ * @author isacc
+ */
+public class VarArgs {
+    
+    static void vaTest(int... v){
+    
+        System.out.println("Number of args: " + v.length + " Contents: ");
+    
+        for (int i : v) {
+            
+            System.out.print(i + " ");
+            
+            System.out.println();
+        }
+    }//fim vaTest
+    
+     static void vaTest(boolean... v){
+    
+        System.out.println("Number of args: " + v.length + " Contents: ");
+    
+        for (boolean i : v) {
+            
+            System.out.print(i + " ");
+            
+            System.out.println();
+        }
+    }//fim vaTest
+    
+    
+    
+    //o varArg deve ser o último parâmetro
+    int doIt(int a, int b, double c, int... vals){
+    
+    }
+    
+    
+    
+    public static void main(String[] args) {
+        
+        // Notice how vaTest() can be called with a varArgs.
+        
+        vaTest(10);      // 1 argumento
+        vaTest(1,2,3);   // 3 argumentos
+        vaTest();        // 0 argumentos
+        
+    }
+}
