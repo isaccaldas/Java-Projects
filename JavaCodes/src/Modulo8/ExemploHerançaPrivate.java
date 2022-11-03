bbf3b0cf437d03f14a58ae159acb78a5239639d7
isaccaldas <34041465+isaccaldas@users.noreply.github.com>
2022-11-03 08:34:14 -0300

    Create ExemploHerançaPrivate.java

---
+++ JavaCodes/src/Modulo8/ExemploHerançaPrivate.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modulo8;
+
+/**
+ *
+ * @author isacc
+ */
+public class ExemploHerançaPrivate {
+    
+    int i; // Acesso default
+    private int j; //privado para a classe ExemploHerançaPrivate
+    
+    void setij(int x, int y){
+    
+        i = x;
+        j = y;
+    }
+        
+}
+
+class Filha extends ExemploHerançaPrivate{
+    
+        int total;
+        
+        void sum(){
+        
+            total = i+j; //Erro j não é acessível aqui pois é privado na classe pai.
+        }
+    
+    }
