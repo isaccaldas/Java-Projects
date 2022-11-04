bb459660a4d32c0d29eb4094cf5c9402040ae241
isaccaldas <34041465+isaccaldas@users.noreply.github.com>
2022-11-04 09:31:14 -0300

    Sobrescrita

--- JavaCodes/src/Modulo8/Sobrescrita.java
+++ JavaCodes/src/Modulo8/Sobrescrita.java
@@ -6,7 +6,7 @@ package Modulo8;
  */
 public class Sobrescrita {
     
-    public static void main(String[] args) {
+    public static void main(String... args) {
         B subOb = new B(1,2,3);
         subOb.show();
     }
